0476838e069526a36b2a322fccc1c37d613e2cc3
imen-chabchoub <135607768+imen-chabchoub@users.noreply.github.com>
2025-11-13 20:30:23 +0100

    Delete .history directory

--- .history/MobileApp/frontend/app/(tabs)/index_20251113181944.tsx
+++
@@ -1,8 +0,0 @@
-// app/index.tsx
-import { useEffect } from 'react';
-import { Redirect } from 'expo-router';
-
-export default function Index() {
-  // Rediriger vers la page de login par défaut
-  return <Redirect href ="/LoginScreen" />;
-} 
